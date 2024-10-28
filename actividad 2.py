f692d23fb5f82787d85da68dd625a6f48e4bc3a5
Mariaavalos <164431989+Mariaavalos@users.noreply.github.com>
2024-10-28 08:39:54 -0300

    actividad 2

---
+++ actividad 2.py
@@ -0,0 +1,13 @@
+edad=0
+fdn=0
+fac=0
+import datetime
+
+hoy=datetime.date.today(2024,6,6)
+
+
+nacim=datetime.date.today(2008,4,2)
+                        
+edad=hoy-nacim
+print("su edad es: ",edad)
+
