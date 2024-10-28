04e2fa80ccb96ca584599008730756342f6c48cd
Mariaavalos <164431989+Mariaavalos@users.noreply.github.com>
2024-10-28 08:39:08 -0300

    actividad 1

---
+++ actividad 1.py
@@ -0,0 +1,16 @@
+
+
+nombre=""
+edad=0
+nac=0
+actu=0
+
+print("ingrese su nombre")
+nombre=(input())
+print("hola",nombre)
+print("¿Cuál es su año de nacimiento?")
+nac=int(input())
+print("ingrese el año actual")
+actu=int(input())
+edad=actu-nac
+print("su edad es ",edad)
