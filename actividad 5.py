c83db9ec962a47b0b4bdc3eacc048b546d4ad38f
Mariaavalos <164431989+Mariaavalos@users.noreply.github.com>
2024-10-29 08:20:02 -0300

    actividad 5 completa

--- actividad 5.py
+++ actividad 5.py
@@ -1,1 +1,20 @@
 # cifrado cesar tarannn
+texto=""
+desplazamiento=0
+print("ingrese el texto a cifrar")
+texto=input()
+print("ingrese el desplazamiento")
+desplazamiento=int(input())
+
+def cifrar(texto,desplazamiento):
+    resultado=""
+    for i in (texto):
+        letra=ord(i) + desplazamiento
+        if letra>90:
+            letra=letra-26
+        else:chr(letra)
+        resultado=resultado + chr(letra)
+    return resultado
+
+mensaje_cifrado=cifrar(texto,desplazamiento)
+print(mensaje_cifrado)
